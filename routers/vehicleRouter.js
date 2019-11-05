b71e7d532436a84d8463a9830e06be6725a7ba57
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-11-05 17:15:33 -0500

    adds error handling if id not found

--- routers/vehicleRouter.js
+++ routers/vehicleRouter.js
@@ -32,7 +32,7 @@ router.delete('/:id', (req, res) => {
     knex('vehicles')
     .where({ id: req.params.id })
     .del()
-    .then(deleted => res.status(200).json(deleted))
+    .then(deleted => deleted === 0 ? res.status(404).json({ message: 'id does not exist'}) : res.status(200).json(deleted))
     .catch(err => res.status(500).json({ error: 'Failed to delete vehicle'}))
 })
 
