f6698067ce46f91c4a2d77087585945d017379b0
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-11-05 17:29:37 -0500

    adds error handling for put

--- routers/vehicleRouter.js
+++ routers/vehicleRouter.js
@@ -24,7 +24,7 @@ router.put('/:id', (req, res) => {
     knex('vehicles')
     .where({ id: req.params.id })
     .update(req.body)
-    .then(updated => res.status(200).json(updated))
+    .then(updated => updated === 0 ? res.status(404).json({ message: 'id does not exist'}) : res.status(200).json(updated))
     .catch(err => res.status(500).json({ error: 'Failed to update vehicle' }))
 })
 
